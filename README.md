87052a21021137a536fc7048e1c402e11d56801a
dejorrit <dejorrit@gmail.com>
2019-11-29 16:07:56 +0100

    Updated example code and Readme

--- README.md
+++ README.md
@@ -3,11 +3,11 @@
 This repository contains examples on how to implement Mollie Components.
 
 ## Example 1 (regular labels)
-[HTML](/examples/example1/index.html) | [CSS](/examples/example1/style.css)
+[HTML](/examples/example1/index.html) | [CSS](/examples/example1/style.css) | [Javascript](/examples/example1/script.js)
 
 ![](screenshots/example1.gif)
 
 ## Example 2 (floating labels)
-[HTML](/examples/example2/index.html) | [CSS](/examples/example2/style.css)
+[HTML](/examples/example2/index.html) | [CSS](/examples/example2/style.css) | [Javascript](/examples/example2/script.js)
 
 ![](screenshots/example2.gif)
---
+++ examples/example2/script.js
@@ -0,0 +1,154 @@
+/**
+ * Initialize Mollie Components instance
+ */
+const mollie = Mollie("pfl_saaa222aaa3", {
+  locale: "en_US"
+});
+
+/**
+ * Error containers
+ */
+const cardNumberErrors = document.getElementById("card-number-errors");
+const cardHolderErrors = document.getElementById("card-holder-errors");
+const expiryDateErrors = document.getElementById("expiry-date-errors");
+const verificationCodeErrors = document.getElementById("verification-code-errors");
+const formErrors = document.getElementById("form-errors");
+
+/**
+ * Get elements
+ */
+const form = document.getElementById("mcForm");
+const submitButton = document.getElementById("submit-button");
+
+/**
+ * Create card holder input
+ */
+const cardHolder = mollie.createComponent("cardHolder");
+cardHolder.mount("#card-holder");
+
+cardHolder.addEventListener("change", event => {
+  if (event.error && event.touched) {
+    cardHolderErrors.textContent = event.error;
+  } else {
+    cardHolderErrors.textContent = "";
+  }
+});
+
+/**
+ * Create card number input
+ */
+const cardNumber = mollie.createComponent("cardNumber");
+cardNumber.mount("#card-number");
+
+cardNumber.addEventListener("change", event => {
+  if (event.error && event.touched) {
+    cardNumberErrors.textContent = event.error;
+  } else {
+    cardNumberErrors.textContent = "";
+  }
+});
+
+/**
+ * Create expiry date input
+ */
+const expiryDate = mollie.createComponent("expiryDate");
+expiryDate.mount("#expiry-date");
+
+expiryDate.addEventListener("change", event => {
+  if (event.error && event.touched) {
+    expiryDateErrors.textContent = event.error;
+  } else {
+    expiryDateErrors.textContent = "";
+  }
+});
+
+/**
+ * Create verification code input
+ */
+const verificationCode = mollie.createComponent("verificationCode");
+verificationCode.mount("#verification-code");
+
+verificationCode.addEventListener("change", event => {
+  if (event.error && event.touched) {
+    verificationCodeErrors.textContent = event.error;
+  } else {
+    verificationCodeErrors.textContent = "";
+  }
+});
+
+/**
+ * Disables the form inputs and submit button
+ */
+function disableForm() {
+  submitButton.disabled = true;
+}
+
+/**
+ * Enables the form inputs and submit button
+ */
+function enableForm() {
+  submitButton.disabled = false;
+}
+
+/**
+ * Submit handler
+ */
+form.addEventListener("submit", async event => {
+  event.preventDefault();
+  disableForm();
+
+  // Reset possible form errors
+  formErrors.textContent = "";
+
+  // Get a payment token
+  const { token, error } = await mollie.createToken();
+
+  if (error) {
+    enableForm();
+    formErrors.textContent = error.message;
+    return;
+  }
+
+  // Add token to the form
+  const tokenInput = document.createElement("input");
+  tokenInput.setAttribute("name", "token");
+  tokenInput.setAttribute("type", "hidden");
+  tokenInput.setAttribute("value", token);
+
+  form.appendChild(tokenInput);
+
+  // Re-submit form to the server
+  form.submit();
+});
+
+
+/**
+ * For the floating labels to work we need some extra event listeners
+ * to set proper classes on the form-group elements: `has-focus` and `is-dirty`
+ */
+
+function toggleFieldDirtyClass(fieldName, { dirty }) {
+  const element = document.getElementById(fieldName);
+  element.parentNode.classList.toggle('is-dirty', dirty);
+}
+
+function toggleFieldFocusClass(fieldName, hasFocus) {
+  const element = document.getElementById(fieldName);
+  element.parentNode.classList.toggle('has-focus', hasFocus);
+}
+
+cardHolder.addEventListener("change", event => toggleFieldDirtyClass('card-holder', event));
+cardHolder.addEventListener("focus", () => toggleFieldFocusClass('card-holder', true));
+cardHolder.addEventListener("blur", () => toggleFieldFocusClass('card-holder', false));
+
+cardNumber.addEventListener("change", event => toggleFieldDirtyClass('card-number', event));
+cardNumber.addEventListener("focus", () => toggleFieldFocusClass('card-number', true));
+cardNumber.addEventListener("blur", () => toggleFieldFocusClass('card-number', false));
+
+expiryDate.addEventListener("change", event => toggleFieldDirtyClass('expiry-date', event));
+expiryDate.addEventListener("focus", () => toggleFieldFocusClass('expiry-date', true));
+expiryDate.addEventListener("blur", () => toggleFieldFocusClass('expiry-date', false));
+
+verificationCode.addEventListener("change", event => toggleFieldDirtyClass('verification-code', event));
+verificationCode.addEventListener("focus", () => toggleFieldFocusClass('verification-code', true));
+verificationCode.addEventListener("blur", () => toggleFieldFocusClass('verification-code', false));

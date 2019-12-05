c1cd536d8a4ca50774794daeb2845bc0a35ce89b
dejorrit <dejorrit@gmail.com>
2019-12-05 09:32:51 +0100

    Added testmode parameter to examples

--- example-2/script.js
+++ example-2/script.js
@@ -4,7 +4,8 @@
 const mollie = Mollie(
   "pfl_HgMrHhRAFm", // You can find your Profile ID in the Dashboard (https://www.mollie.com/dashboard/developers/api-keys)
   {
-    locale: "en_US" // Optional. If not provided, we will determine the users' language by looking at the document and/or userAgent.
+    locale: "en_US", // Optional. If not provided, we will determine the users' language by looking at the document and/or userAgent.
+    testmode: false // Set to true to enable test mode.
   }
 );
 

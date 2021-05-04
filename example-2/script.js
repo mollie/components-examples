ba313e7ff273f7ca1d0671df514eb18e3c18ca56
Angelo Abdoelsamath <angelo.a@me.com>
2021-05-04 15:55:33 +0200

    Example 2

--- example-2/script.js
+++ example-2/script.js
@@ -1,13 +1,29 @@
 /**
  * Initialize Mollie Components instance
  */
-const mollie = Mollie(
+ const mollie = Mollie(
   "pfl_HgMrHhRAFm", // You can find your Profile ID in the Dashboard (https://www.mollie.com/dashboard/developers/api-keys)
   {
     locale: "en_US", // Optional. If not provided, we will determine the users' language by looking at the document and/or userAgent.
     testmode: false // Set to true to enable test mode.
   }
 );
+
+/**
+ * styles
+ */
+var options = {
+  styles: {
+    base: {
+      color: "rgba(255,255,255,0.8)",
+      fontSize: "16px",
+      fontWeight: "500"
+    },
+    "::placeholder": {
+      color: "rgba(255,255,255,0.2)"
+    }
+  }
+};
 
 /**
  * Get elements
@@ -19,12 +35,12 @@ const submitButton = document.getElementById("submit-button");
 /**
  * Create card holder input
  */
-const cardHolder = mollie.createComponent("cardHolder");
+const cardHolder = mollie.createComponent("cardHolder", options);
 cardHolder.mount("#card-holder");
 
 const cardHolderError = document.getElementById("card-holder-error");
 
-cardHolder.addEventListener("change", event => {
+cardHolder.addEventListener("change", (event) => {
   if (event.error && event.touched) {
     cardHolderError.textContent = event.error;
   } else {
@@ -35,12 +51,12 @@ cardHolder.addEventListener("change", event => {
 /**
  * Create card number input
  */
-const cardNumber = mollie.createComponent("cardNumber");
+const cardNumber = mollie.createComponent("cardNumber", options);
 cardNumber.mount("#card-number");
 
 const cardNumberError = document.getElementById("card-number-error");
 
-cardNumber.addEventListener("change", event => {
+cardNumber.addEventListener("change", (event) => {
   if (event.error && event.touched) {
     cardNumberError.textContent = event.error;
   } else {
@@ -51,12 +67,12 @@ cardNumber.addEventListener("change", event => {
 /**
  * Create expiry date input
  */
-const expiryDate = mollie.createComponent("expiryDate");
+const expiryDate = mollie.createComponent("expiryDate", options);
 expiryDate.mount("#expiry-date");
 
 const expiryDateError = document.getElementById("expiry-date-error");
 
-expiryDate.addEventListener("change", event => {
+expiryDate.addEventListener("change", (event) => {
   if (event.error && event.touched) {
     expiryDateError.textContent = event.error;
   } else {
@@ -67,12 +83,14 @@ expiryDate.addEventListener("change", event => {
 /**
  * Create verification code input
  */
-const verificationCode = mollie.createComponent("verificationCode");
+const verificationCode = mollie.createComponent("verificationCode", options);
 verificationCode.mount("#verification-code");
 
-const verificationCodeError = document.getElementById("verification-code-error");
+const verificationCodeError = document.getElementById(
+  "verification-code-error"
+);
 
-verificationCode.addEventListener("change", event => {
+verificationCode.addEventListener("change", (event) => {
   if (event.error && event.touched) {
     verificationCodeError.textContent = event.error;
   } else {
@@ -97,7 +115,7 @@ function enableForm() {
 /**
  * Submit handler
  */
-form.addEventListener("submit", async event => {
+form.addEventListener("submit", async (event) => {
   event.preventDefault();
   disableForm();
 
@@ -124,35 +142,3 @@ form.addEventListener("submit", async event => {
   // Re-submit form to the server
   form.submit();
 });
-
-
-/**
- * For the floating labels to work we need some extra event listeners
- * to set proper classes on the form-group elements: `has-focus` and `is-dirty`
- */
-
-function toggleFieldDirtyClass(fieldName, dirty) {
-  const element = document.getElementById(fieldName);
-  element.parentNode.classList.toggle('is-dirty', dirty);
-}
-
-function toggleFieldFocusClass(fieldName, hasFocus) {
-  const element = document.getElementById(fieldName);
-  element.parentNode.classList.toggle('has-focus', hasFocus);
-}
-
-cardHolder.addEventListener("change", event => toggleFieldDirtyClass('card-holder', event.dirty));
-cardHolder.addEventListener("focus", () => toggleFieldFocusClass('card-holder', true));
-cardHolder.addEventListener("blur", () => toggleFieldFocusClass('card-holder', false));
-
-cardNumber.addEventListener("change", event => toggleFieldDirtyClass('card-number', event.dirty));
-cardNumber.addEventListener("focus", () => toggleFieldFocusClass('card-number', true));
-cardNumber.addEventListener("blur", () => toggleFieldFocusClass('card-number', false));
-
-expiryDate.addEventListener("change", event => toggleFieldDirtyClass('expiry-date', event.dirty));
-expiryDate.addEventListener("focus", () => toggleFieldFocusClass('expiry-date', true));
-expiryDate.addEventListener("blur", () => toggleFieldFocusClass('expiry-date', false));
-
-verificationCode.addEventListener("change", event => toggleFieldDirtyClass('verification-code', event.dirty));
-verificationCode.addEventListener("focus", () => toggleFieldFocusClass('verification-code', true));
-verificationCode.addEventListener("blur", () => toggleFieldFocusClass('verification-code', false));

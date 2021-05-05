28ed20c5e306e5f74258fa00752eba7a34096f83
Angelo Abdoelsamath <angelo.a@me.com>
2021-05-05 15:06:52 +0200

    Example 3

---
+++ example-3/script.js
@@ -0,0 +1,145 @@
+/**
+ * Initialize Mollie Components instance
+ */
+ const mollie = Mollie(
+  "pfl_HgMrHhRAFm", // You can find your Profile ID in the Dashboard (https://www.mollie.com/dashboard/developers/api-keys)
+  {
+    locale: "en_US", // Optional. If not provided, we will determine the users' language by looking at the document and/or userAgent.
+    testmode: false // Set to true to enable test mode.
+  }
+);
+
+/**
+ * styles
+ */
+var options = {
+  styles: {
+    base: {
+      color: "#173D68",
+      fontSize: "16px",
+      fontWeight: "500"
+    },
+    "::placeholder": {
+      color: "#A3B2C6",
+      fontSize: "16px"
+    }
+  }
+};
+
+/**
+ * Get elements
+ */
+const form = document.getElementById("mcForm");
+const formError = document.getElementById("form-error");
+const submitButton = document.getElementById("submit-button");
+
+/**
+ * Create card holder input
+ */
+const cardHolder = mollie.createComponent("cardHolder", options);
+cardHolder.mount("#card-holder");
+
+const cardHolderError = document.getElementById("card-holder-error");
+
+cardHolder.addEventListener("change", (event) => {
+  if (event.error && event.touched) {
+    cardHolderError.textContent = event.error;
+  } else {
+    cardHolderError.textContent = "";
+  }
+});
+
+/**
+ * Create card number input
+ */
+const cardNumber = mollie.createComponent("cardNumber", options);
+cardNumber.mount("#card-number");
+
+const cardNumberError = document.getElementById("card-number-error");
+
+cardNumber.addEventListener("change", (event) => {
+  if (event.error && event.touched) {
+    cardNumberError.textContent = event.error;
+  } else {
+    cardNumberError.textContent = "";
+  }
+});
+
+/**
+ * Create expiry date input
+ */
+const expiryDate = mollie.createComponent("expiryDate", options);
+expiryDate.mount("#expiry-date");
+
+const expiryDateError = document.getElementById("expiry-date-error");
+
+expiryDate.addEventListener("change", (event) => {
+  if (event.error && event.touched) {
+    expiryDateError.textContent = event.error;
+  } else {
+    expiryDateError.textContent = "";
+  }
+});
+
+/**
+ * Create verification code input
+ */
+const verificationCode = mollie.createComponent("verificationCode", options);
+verificationCode.mount("#verification-code");
+
+const verificationCodeError = document.getElementById(
+  "verification-code-error"
+);
+
+verificationCode.addEventListener("change", (event) => {
+  if (event.error && event.touched) {
+    verificationCodeError.textContent = event.error;
+  } else {
+    verificationCodeError.textContent = "";
+  }
+});
+
+/**
+ * Disables the form inputs and submit button
+ */
+function disableForm() {
+  submitButton.disabled = true;
+}
+
+/**
+ * Enables the form inputs and submit button
+ */
+function enableForm() {
+  submitButton.disabled = false;
+}
+
+/**
+ * Submit handler
+ */
+form.addEventListener("submit", async (event) => {
+  event.preventDefault();
+  disableForm();
+
+  // Reset possible form error
+  formError.textContent = "";
+
+  // Get a payment token
+  const { token, error } = await mollie.createToken();
+
+  if (error) {
+    enableForm();
+    formError.textContent = error.message;
+    return;
+  }
+
+  // Add token to the form
+  const tokenInput = document.createElement("input");
+  tokenInput.setAttribute("name", "token");
+  tokenInput.setAttribute("type", "hidden");
+  tokenInput.setAttribute("value", token);
+
+  form.appendChild(tokenInput);
+
+  // Re-submit form to the server
+  form.submit();
+});
